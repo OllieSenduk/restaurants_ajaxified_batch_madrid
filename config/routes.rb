21c60d20a79267dc68c64da3bc12d86ea61b3aa1
Oliver Senduk <oliver@ichistudios.com>
2020-02-28 09:48:10 +0100

    Added controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :show] do
+    resources :reviews
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
